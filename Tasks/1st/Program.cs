540dc4c250aa93a246e078b41ff0163ca6693a59
Evgeniy <zheinirk38@gmail.com>
2022-09-09 22:43:34 +0800

    1st task

---
+++ Tasks/1st/Program.cs
@@ -0,0 +1,22 @@
+﻿// Является ли число палиндромом
+try {
+    Console.WriteLine("Enter the integer number: ");
+    string? num = Console.ReadLine();
+    Console.WriteLine(checkPalindrom(num));
+} catch {
+    Console.WriteLine("Try again!");
+}
+
+String checkPalindrom(string num) {
+    // string num = number.ToString();
+    string result = "";
+    for(int i = 0; i < num.Length / 2; i++) {
+        if(num[i] == num[num.Length - 1 - i]) {
+            result = $"The number {num} is palindrome!";
+        } else {
+            result = $"The number {num} is NOT palindrome!";
+        }
+    }
+
+    return result;
+}
