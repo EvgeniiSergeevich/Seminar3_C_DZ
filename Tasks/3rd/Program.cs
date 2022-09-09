9b91a43cbb37b9c6433a667ef74cc43fd9b10368
Evgeniy <zheinirk38@gmail.com>
2022-09-09 23:07:18 +0800

    3rd task

---
+++ Tasks/3rd/Program.cs
@@ -0,0 +1,16 @@
+﻿// Таблица кубов чисел
+try {
+    Console.WriteLine("Enter an integer number: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(coube(x));
+} catch {
+    Console.WriteLine("Enter a number!");
+}
+
+string coube(int x) {
+    string coubeX = "";
+    for (int i = 1; i <= x; i++) {
+        coubeX += i*i*i + ", ";
+    }
+    return coubeX;
+}
